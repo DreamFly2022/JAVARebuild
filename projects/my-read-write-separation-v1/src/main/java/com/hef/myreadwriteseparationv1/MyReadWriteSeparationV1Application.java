4a4a2cf6f739f6964ceab8cdb183ccd25a4147a7
Li Fei <hefrankeleyn@gamil.com>
2022-01-15 21:12:53 +0800

    半同步复制：一主两从搭建
    搭建一主两从

---
+++ projects/my-read-write-separation-v1/src/main/java/com/hef/myreadwriteseparationv1/MyReadWriteSeparationV1Application.java
@@ -0,0 +1,13 @@
+package com.hef.myreadwriteseparationv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyReadWriteSeparationV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyReadWriteSeparationV1Application.class, args);
+	}
+
+}
